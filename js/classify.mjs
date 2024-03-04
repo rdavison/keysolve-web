636f4061eabc44b7b102f209ce76c25207dece36
Richard Davison <richard.neil.davison@gmail.com>
2024-03-03 23:00:49 -0500

    there be dragons

--- js/classify.mjs
+++ js/classify.mjs
@@ -6,13 +6,34 @@ export const STANDARD = [
 ]
 
 export const ANGLE = [
+    0, 1, 2, 2, 3, 6, 6, 7, 8, 9,
     0, 1, 2, 3, 3, 6, 6, 7, 8, 9,
-    0, 1, 2, 3, 3, 6, 6, 7, 8, 9,
-    1, 2, 3, 3, 3, 6, 6, 7, 8, 9,
+    0, 2, 3, 3, 3, 6, 6, 7, 8, 9,
     5, 5, 5
 ]
 
+const STAGGER = true
+
 export let FINGER_MAP = STANDARD
+
+function keypos(idx) {
+    let row = null
+    let col = idx % 10
+    if (idx >= 0 && idx < 10) {
+        row = 0
+    } else if (idx >= 10 && idx < 20) {
+        row = 1
+        col += 0.25
+    } else if (idx >= 20 && idx < 30) {
+        row = 2
+        col += 0.75
+    }
+    if (idx === 29) {
+        row = 1
+        col += 0.5
+    }
+    return [row, col]
+}
 
 export function angle(bool) {
     if (bool) {
@@ -26,6 +47,14 @@ export function angle(bool) {
 
 function finger(idx) {
     return FINGER_MAP[idx]
+}
+
+function justfinger(idx) {
+    if (FINGER_MAP[idx] < 5) {
+        return FINGER_MAP[idx]
+    } else {
+        return FINGER_MAP[idx] - ((FINGER_MAP[idx] % 5) * 2 + 1)
+    }
 }
 
 function column(idx) {
@@ -85,49 +114,253 @@ function bigrams(key) {
         return buckets
     }
     
-    if (
-        hand(key[0]) == hand(key[1]) &&
-        (
-            [4, 5].includes(column(key[0])) ||
-            [4, 5].includes(column(key[1]))
-        ) &&
-        (
-            [2, 7].includes(column(key[0])) ||
-            [2, 7].includes(column(key[1]))
-        )
-    ) {
-        buckets.push('LS')
+    if (hand(key[0]) === hand(key[1])) {
+        if (Math.abs(justfinger(key[0]) - justfinger(key[1])) === 1) {
+            const d1 = keypos(key[0])
+            const d2 = keypos(key[1])
+            if (Math.abs(d1[1] - d2[1]) > 1.5) {
+                // if (key[0] === 25 && key[1] === 17) {
+                // }
+                console.log('LS',key[0],key[1])
+                buckets.push('LS')
+            }
+        }
+
+
+
+        // if (FINGER_MAP === STANDARD) {
+        //     if (
+        //         (
+        //             [4, 5].includes(column(key[0])) ||
+        //             [4, 5].includes(column(key[1]))
+        //         ) &&
+        //         (
+        //             [2, 7].includes(column(key[0])) ||
+        //             [2, 7].includes(column(key[1]))
+        //         )
+        //     ) {
+        //         buckets.push('LS')
+        //     }
+        // } else if (FINGER_MAP === ANGLE) {
+        //     if (
+        //         (
+        //             [4, 5].includes(column(key[0])) ||
+        //             [4, 5].includes(column(key[1]))
+        //         ) &&
+        //         (
+        //             [2, 7].includes(column(key[0])) ||
+        //             [2, 7].includes(column(key[1]))
+        //         )
+        //     ) {
+        //         buckets.push('LS')
+        //     } else if (
+        //         (
+        //             key[0] === 21 && [4,14,23,24].includes(key[1])
+        //         ) ||
+        //         (
+        //             [4,14,23,24].includes(key[0]) && key[1] === 21
+        //         ) 
+        //     ) {
+
+        //         buckets.push('LS')
+        //     }
+        // }
+
     }
 
-    if (
-        (
-            row(key[0]) - row(key[1]) == -1 &&
-            hand(key[0]) == hand(key[1]) &&
-            [1, 2, 7, 8].includes(finger(key[1]))
-        ) ||
-        (
-            row(key[0]) - row(key[1]) == 1 &&
-            hand(key[0]) == hand(key[1]) &&
-            [1, 2, 7, 8].includes(finger(key[0]))
-        )
-    ) {
-        buckets.push('HS')
+    // if (
+    //     (
+    //         row(key[1]) - row(key[0]) == -1 &&
+    //         hand(key[0]) == hand(key[1]) &&
+    //         [1, 2, 7, 8].includes(finger(key[1]))
+    //     ) ||
+    //     (
+    //         row(key[1]) - row(key[0]) == 1 &&
+    //         hand(key[0]) == hand(key[1]) &&
+    //         [1, 2, 7, 8].includes(finger(key[0]))
+    //     )
+    // ) {
+    //     buckets.push('HS')
+    // }
+
+    if (hand(key[0]) == hand(key[1])) {
+        if (justfinger(key[0]) == 0 && justfinger(key[1]) == 1) {
+            if (row(key[1]) - row(key[0]) == -2) {
+                buckets.push('FS')
+            } else if (row(key[1]) - row(key[0]) == -1) {
+            } else if (row(key[1]) - row(key[0]) == 0) {
+                // buckets.push('SR')
+            } else if (row(key[1]) - row(key[0]) == 1) {
+                buckets.push('HS')
+            } else if (row(key[1]) - row(key[0]) == 2) {
+                buckets.push('FS')
+            }
+        } else if (justfinger(key[0]) == 0 && justfinger(key[1]) == 2) {
+            if (row(key[1]) - row(key[0]) == -2) {
+                buckets.push('HS')
+            } else if (row(key[1]) - row(key[0]) == -1) {
+            } else if (row(key[1]) - row(key[0]) == 0) {
+                // buckets.push('SR')
+            } else if (row(key[1]) - row(key[0]) == 1) {
+                buckets.push('HS')
+            } else if (row(key[1]) - row(key[0]) == 2) {
+                buckets.push('FS')
+            }
+        } else if (justfinger(key[0]) == 0 && justfinger(key[1]) == 3) {
+            if (row(key[1]) - row(key[0]) == -2) {
+                buckets.push('FS')
+            } else if (row(key[1]) - row(key[0]) == -1) {
+                buckets.push('HS')
+            } else if (row(key[1]) - row(key[0]) == 0) {
+                // buckets.push('SR')
+            } else if (row(key[1]) - row(key[0]) == 1) {
+            } else if (row(key[1]) - row(key[0]) == 2) {
+            }
+        } else if (justfinger(key[0]) == 1 && justfinger(key[1]) == 0) {
+            if (row(key[1]) - row(key[0]) == -2) {
+                buckets.push('FS')
+            } else if (row(key[1]) - row(key[0]) == -1) {
+                buckets.push('HS')
+            } else if (row(key[1]) - row(key[0]) == 0) {
+                // buckets.push('SR')
+            } else if (row(key[1]) - row(key[0]) == 1) {
+            } else if (row(key[1]) - row(key[0]) == 2) {
+                buckets.push('FS')
+            }
+        } else if (justfinger(key[0]) == 1 && justfinger(key[1]) == 2) {
+            if (row(key[1]) - row(key[0]) == -2) {
+                buckets.push('FS')
+            } else if (row(key[1]) - row(key[0]) == -1) {
+            } else if (row(key[1]) - row(key[0]) == 0) {
+                // buckets.push('SR')
+            } else if (row(key[1]) - row(key[0]) == 1) {
+                buckets.push('HS')
+            } else if (row(key[1]) - row(key[0]) == 2) {
+                buckets.push('FS')
+            }
+        } else if (justfinger(key[0]) == 1 && justfinger(key[1]) == 3) {
+            if (row(key[1]) - row(key[0]) == -2) {
+                buckets.push('FS')
+            } else if (row(key[1]) - row(key[0]) == -1) {
+                buckets.push(buckets.includes('LS') ? 'FS' : 'HS')
+            } else if (row(key[1]) - row(key[0]) == 0) {
+                // buckets.push('SR')
+            } else if (row(key[1]) - row(key[0]) == 1) {
+            } else if (row(key[1]) - row(key[0]) == 2) {
+                if (STAGGER && key[1] == 24) {
+                    buckets.push('FS')
+                } else if (STAGGER && key[1] == 23) {
+                    buckets.push('HS')
+                }
+            }
+        } else if (justfinger(key[0]) == 2 && justfinger(key[1]) == 0) {
+            if (row(key[1]) - row(key[0]) == -2) {
+                buckets.push('FS')
+            } else if (row(key[1]) - row(key[0]) == -1) {
+                buckets.push('HS')
+            } else if (row(key[1]) - row(key[0]) == 0) {
+                // buckets.push('SR')
+            } else if (row(key[1]) - row(key[0]) == 1) {
+            } else if (row(key[1]) - row(key[0]) == 2) {
+                buckets.push('HS')
+            }
+        } else if (justfinger(key[0]) == 2 && justfinger(key[1]) == 1) {
+            if (row(key[1]) - row(key[0]) == -2) {
+                buckets.push('FS')
+            } else if (row(key[1]) - row(key[0]) == -1) {
+                buckets.push('HS')
+            } else if (row(key[1]) - row(key[0]) == 0) {
+                // buckets.push('SR')
+            } else if (row(key[1]) - row(key[0]) == 1) {
+            } else if (row(key[1]) - row(key[0]) == 2) {
+                buckets.push('FS')
+            }
+        } else if (justfinger(key[0]) == 2 && justfinger(key[1]) == 3) {
+            if (row(key[1]) - row(key[0]) == -2) {
+                buckets.push('FS')
+            } else if (row(key[1]) - row(key[0]) == -1) {
+                buckets.push(buckets.includes('LS') ? 'FS' : 'HS')
+            } else if (row(key[1]) - row(key[0]) == 0) {
+                // buckets.push('SR')
+            } else if (row(key[1]) - row(key[0]) == 1) {
+                if (STAGGER && hand(key[0] == 0) && buckets.includes('LS')) {
+                    buckets.push('FS')
+                }
+            } else if (row(key[1]) - row(key[0]) == 2) {
+                if (STAGGER && hand(key[0] == 0)) {
+                    if (buckets.includes('LS')) {
+                        buckets.push('FS')    
+                    }
+                    else {
+                        buckets.push('HS')
+                    }
+                }
+            }
+        } else if (justfinger(key[0]) == 3 && justfinger(key[1]) == 0) {
+            if (row(key[1]) - row(key[0]) == -2) {
+            } else if (row(key[1]) - row(key[0]) == -1) {
+            } else if (row(key[1]) - row(key[0]) == 0) {
+                // buckets.push('SR')
+            } else if (row(key[1]) - row(key[0]) == 1) {
+                buckets.push('HS')
+            } else if (row(key[1]) - row(key[0]) == 2) {
+                buckets.push('FS')
+            }
+        } else if (justfinger(key[0]) == 3 && justfinger(key[1]) == 1) {
+            if (row(key[1]) - row(key[0]) == -2) {
+                if (STAGGER && key[0] == 24) {
+                    console.log("asdfjkl;")
+                    buckets.push('FS')
+                } else if (STAGGER && key[0] == 23) {
+                    console.log("ineahtsr")
+                    buckets.push('HS')
+                }
+            } else if (row(key[1]) - row(key[0]) == -1) {
+            } else if (row(key[1]) - row(key[0]) == 0) {
+                // buckets.push('SR')
+            } else if (row(key[1]) - row(key[0]) == 1) {
+                buckets.push(buckets.includes('LS') ? 'FS' : 'HS')
+            } else if (row(key[1]) - row(key[0]) == 2) {
+                buckets.push('FS')
+            }
+        } else if (justfinger(key[0]) == 3 && justfinger(key[1]) == 2) {
+            if (row(key[1]) - row(key[0]) == -2) {
+                if (STAGGER && hand(key[0] == 0)) {
+                    if (buckets.includes('LS')) {
+                        buckets.push('FS')    
+                    }
+                    else {
+                        buckets.push('HS')
+                    }
+                }
+            } else if (row(key[1]) - row(key[0]) == -1) {
+                if (STAGGER && hand(key[0] == 0) && buckets.includes('LS')) {
+                    buckets.push('FS')
+                }
+            } else if (row(key[1]) - row(key[0]) == 0) {
+                // buckets.push('SR')
+            } else if (row(key[1]) - row(key[0]) == 1) {
+                buckets.push(buckets.includes('LS') ? 'FS' : 'HS')
+            } else if (row(key[1]) - row(key[0]) == 2) {
+                buckets.push('FS')
+            }
+        }
     }
 
-    if (
-        (
-            row(key[0]) - row(key[1]) == -2 &&
-            hand(key[0]) == hand(key[1]) &&
-            [1, 2, 7, 8].includes(finger(key[1]))
-        ) ||
-        (
-            row(key[0]) - row(key[1]) == 2 &&
-            hand(key[0]) == hand(key[1]) &&
-            [1, 2, 7, 8].includes(finger(key[0]))
-        )
-    ) {
-        buckets.push('FS')
-    }
+    // if (
+    //     (
+    //         row(key[1]) - row(key[0]) == -2 &&
+    //         hand(key[0]) == hand(key[1]) &&
+    //         [1, 2, 7, 8].includes(finger(key[1]))
+    //     ) ||
+    //     (
+    //         row(key[1]) - row(key[0]) == 2 &&
+    //         hand(key[0]) == hand(key[1]) &&
+    //         [1, 2, 7, 8].includes(finger(key[0]))
+    //     )
+    // ) {
+    //     buckets.push('FS')
+    // }
 
     return buckets
 }
--- js/stats.mjs
+++ js/stats.mjs
@@ -15,6 +15,31 @@ export async function init() {
     TRIGRAMS = await (await fetch('corpora/trigrams.json')).json()
 }
 
+function keypos(idx) {
+    let row = null
+    let col = idx % 10
+    if (idx >= 0 && idx < 10) {
+        row = 0
+    } else if (idx >= 10 && idx < 20) {
+        row = 1
+        col += 0.25
+    } else if (idx >= 20 && idx < 30) {
+        row = 2
+        col += 0.75
+    }
+    if (idx === 29) {
+        row = 1
+        col += 0.5
+    }
+    return [row, col]
+}
+
+function twokeydist(k1, k2) {
+    let p1 = keypos(k1)
+    let p2 = keypos(k2)
+    return Math.sqrt(Math.pow(p2[1] - p1[1], 2) + Math.pow(p2[0] - p1[0], 2))
+}
+
 export function analyze() {
     const letters = board.layout()
     const layout = {}
@@ -42,21 +67,36 @@ export function analyze() {
         let curr = {}
         let total = 0
 
-        for (const [gram, count] of Object.entries(grams)) {
+        for (let [gram, count] of Object.entries(grams)) {
             const key = [...gram].map(x => layout[x])
-            total += count
             
             if (key.indexOf(undefined) !== -1) {
                 continue
             }
+
+            total += count
             
             const stats = classify(key)
 
-            for (let stat of stats) {
-                stat = stat + suffix
-
+            for (let stat_ of stats) {
+                let stat = stat_ + suffix
+                let dist = twokeydist(key[0], key[1])
+                // let dist = 1
+                
                 curr[stat] ??= 0
-                curr[stat] += count
+
+                if (key.length === 2) {
+                    if (stat_ === "LS") {
+                        curr[stat] += count * dist
+                    } else if (stat_ === "SF") {
+                        curr[stat] += count * dist
+                    } else {
+                        curr[stat] += count
+                    }
+                } else {
+                    curr[stat] += count
+                }
+                // curr[stat] += count
             }
         }
 
